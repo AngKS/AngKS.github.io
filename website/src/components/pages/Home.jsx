7c3e52efc6fa92ff7035c43c1126abdfcc6b5d58
AKS <60446729+AngKS@users.noreply.github.com>
2021-08-17 14:07:30 +0800

    Finished building css grid boxes

--- website/src/components/pages/Home.jsx
+++ website/src/components/pages/Home.jsx
@@ -6,11 +6,36 @@ import "../stylesheets/App.css"
 class Home extends Component {
     render() {
         return (
-            <div className='center-container h-100 shadow shadow-md bg-yellow'>
+            <div className='center-container position-relative shadow shadow-md' style={{'height' : '100vh', 'width' : '100%'}}>
                 
-            <section>
-                
-            </section>
+            <div className="center-container">
+                <section className="layout w-75">
+                    <div className="card card-A">
+                        A
+                    </div>
+                    <div className="card card-B">
+                        
+                    </div>
+                    <div className="card card-C">
+                        C
+                    </div>
+                    <div className="card card-D">
+                        D
+                    </div>
+                    <div className="card card-E">
+                        E
+                    </div>
+                    <div className="card card-F">
+                        F
+                    </div>
+                    <div className="card card-G">
+                        G
+                    </div>
+                    <div className="card card-H">
+                        H
+                    </div>
+                </section>
+            </div>
 
 
             </div>
