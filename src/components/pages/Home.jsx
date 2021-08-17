cbc42469f42b86927d6a07b0d7d796deeb6fcc37
AKS <60446729+AngKS@users.noreply.github.com>
2021-08-17 14:51:57 +0800

    Added images

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,23 +1,29 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import "./Home.css"
 import "../stylesheets/App.css"
 
-class Home extends Component {
-    render() {
-        return (
-            <div className='center-container position-relative shadow shadow-md' style={{'height' : '100vh', 'width' : '100%'}}>
-                
+import Panda from '../assets/520x520.png'
+import Face from '../assets/face.png'
+
+function Home() {
+
+    const [check, setCheck] = useState(false)
+    return (
+        <div className='center-container position-relative shadow shadow-md' style={{ 'height': '100vh', 'width': '100%' }}>
+
             <div className="center-container">
                 <section className="layout w-75">
                     <div className="card card-A">
                         A
                     </div>
                     <div className="card card-B">
-                        
+                        <img src={check ? Face : Panda} alt="" className="img-fluid shadow shadow-md thumbnail w-50 mx-auto rounded-circle" onMouseDown={e => setCheck(!check)} />
+                        <h1 className="title">Ang Kah Shin</h1>
+                        <span className="text-muted">@AngKS</span>
                     </div>
                     <div className="card card-C">
-                        C
+                        
                     </div>
                     <div className="card card-D">
                         D
@@ -38,9 +44,9 @@ class Home extends Component {
             </div>
 
 
-            </div>
-        );
-    }
+        </div>
+    );
+
 }
 
 export default Home;
